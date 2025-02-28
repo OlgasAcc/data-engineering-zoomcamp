89d0c782dc69da1a219d44f70041ce1d25542195
olga.krasnozhon3101@gmail.com <olga.krasnozhon3101@gmail.com>
2025-02-28 14:17:17 +0000

    q7 fix

--- 04-analytics-engineering/taxi_rides_ny/models/core/fct_fhv_monthly_zone_traveltime_p90.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_fhv_monthly_zone_traveltime_p90.sql
@@ -3,64 +3,36 @@
 with
     trip_durations as (
         select
-            dispatching_base_num,
-            pickup_datetime,
-            dropoff_datetime,
-            pickup_location_id,
-            dropoff_location_id,
-            year,
-            month,
             pickup_zone,
             dropoff_zone,
             timestamp_diff(dropoff_datetime, pickup_datetime, second) as trip_duration
-        from {{ ref("dim_fhv_trips") }}
+        from `terraform-demo-452019.demo_dataset.dim_fhv_trips`
+        where
+            year = 2019
+            and month = 11
+            and pickup_zone in ('Newark Airport', 'SoHo', 'Yorkville East')
     ),
+
     p90_trip_durations as (
         select
-            year,
-            month,
-            pickup_location_id,
-            dropoff_location_id,
             pickup_zone,
             dropoff_zone,
             approx_quantiles(trip_duration, 100)[offset(90)] as p90_trip_duration
         from trip_durations
-        group by
-            year,
-            month,
-            pickup_location_id,
-            dropoff_location_id,
-            pickup_zone,
-            dropoff_zone
+        group by pickup_zone, dropoff_zone
     ),
-    filtered_trips as (
-        select
-            p90.year,
-            p90.month,
-            p90.pickup_location_id,
-            p90.dropoff_location_id,
-            p90.pickup_zone,
-            p90.dropoff_zone,
-            p90.p90_trip_duration
-        from p90_trip_durations p90
-        where
-            p90.year = 2019
-            and p90.month = 11
-            and p90.pickup_zone in ('Newark Airport', 'SoHo', 'Yorkville East')
-    ),
+
     ranked_trips as (
         select
-            year,
-            month,
-            pickup_location_id,
             pickup_zone,
             dropoff_zone,
             p90_trip_duration,
-            row_number() over (
+            rank() over (
                 partition by pickup_zone order by p90_trip_duration desc
             ) as rank
-        from filtered_trips
+        from p90_trip_durations
     )
+
 select pickup_zone, dropoff_zone, p90_trip_duration
 from ranked_trips
 where rank = 2
